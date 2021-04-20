13064aa3844e493af6b8be7b212ed90dfdcc5a8e
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2021-04-20 10:37:56 -1000

    fixed naming convention & recipe publication

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { Recipes } from '../../api/recipe/recipes';
+import { Recipes } from '../../api/recipe/Recipes';
 
 /* eslint-disable no-console */
 
+//  Adds recipes to Recipes collection.
 function addCreatedRecipe(recipe) {
   if (Meteor.settings.defaultRecipes) {
     console.log(`  Adding: ${recipe.recipeName} (${recipe.recipeEmail})`);
     Recipes.collection.insert(recipe);
   }
-
 }
 
+// Creates default recipes if Recipes collection is empty.
 if (Recipes.collection.find().count() === 0) {
   console.log('Creating default recipes');
   Meteor.settings.defaultRecipes.map(recipe => addCreatedRecipe(recipe));
-
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Profiles } from '../../api/profile/profiles';
-import { Recipes } from '../../api/recipe/recipes';
+import { Recipes } from '../../api/recipe/Recipes';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -17,7 +17,7 @@ Meteor.publish(Profiles.userPublicationName, function () {
 Meteor.publish(Recipes.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
-    return Profiles.collection.find({ recipeEmail: username });
+    return Recipes.collection.find({ recipeEmail: username });
   }
   return this.ready();
 });
