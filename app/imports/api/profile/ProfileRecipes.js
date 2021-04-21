2d3c6fa9f931f7275703a3476d5a602bee3f4fa2
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2021-04-20 22:09:16 -1000

    implemented parts of schema for vendor and profile related collections

--- app/imports/api/profile/ProfileRecipes.js
+++ app/imports/api/profile/ProfileRecipes.js
@@ -3,20 +3,18 @@ import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /**
- * The ProfilesCollection. It encapsulates state and variable values for profile.
+ * The StuffsCollection. It encapsulates state and variable values for stuff.
  */
-class ProfilesCollection {
+class ProfileRecipesCollection {
   constructor() {
     // The name of this collection.
-    this.name = 'ProfilesCollection';
+    this.name = 'ProfileRecipesCollection';
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
-      name: String,
-      bio: String,
-      image: String,
-      owner: String,
+      profile: String,
+      recipeName: String,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
@@ -30,4 +28,4 @@ class ProfilesCollection {
  * The singleton instance of the ProfilesCollection.
  * @type {ProfilesCollection}
  */
-export const Profiles = new ProfilesCollection();
+export const ProfileRecipes = new ProfileRecipesCollection();
--- app/imports/api/vendor/VendorIngedients.js
+++ app/imports/api/vendor/VendorIngedients.js
@@ -3,20 +3,21 @@ import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /**
- * The ProfilesCollection. It encapsulates state and variable values for profile.
+ * The VendorCollection. It encapsulates state and variable values for recipes.
  */
-class ProfilesCollection {
+class VendorCollection {
   constructor() {
     // The name of this collection.
-    this.name = 'ProfilesCollection';
+    this.name = 'VendorCollection';
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
       name: String,
       bio: String,
-      image: String,
-      owner: String,
+      storeHours: String,
+      address: String,
+      email: String,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
@@ -27,7 +28,7 @@ class ProfilesCollection {
 }
 
 /**
- * The singleton instance of the ProfilesCollection.
- * @type {ProfilesCollection}
+ * The singleton instance of the VendorsCollection.
+ * @type {VendorCollection}
  */
-export const Profiles = new ProfilesCollection();
+export const Vendors = new VendorCollection();
